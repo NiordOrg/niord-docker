102ac76b8e874ad5d770972100a06396bf1c80ac
peder280370 <peder@carolus.dk>
2016-11-10 08:08:25 +0100

    Added niord-proxy to the HTTPD docker service

--- 04-niord-httpd/niord-httpd-entrypoint.sh
+++ 04-niord-httpd/niord-httpd-entrypoint.sh
@@ -7,5 +7,8 @@ sed -i 's@KEYCLOAK_NAME@'"$KEYCLOAK_NAME"'@' /usr/local/apache2/conf/niord.conf
 echo "Updating HTTPD configuration with Niord application server: $APPSRV_NAME"
 sed -i 's@APPSRV_NAME@'"$APPSRV_NAME"'@' /usr/local/apache2/conf/niord.conf
 
+echo "Updating HTTPD configuration with Niord Proxy server: $PROXY_NAME"
+sed -i 's@PROXY_NAME@'"$PROXY_NAME"'@' /usr/local/apache2/conf/niord.conf
+
 echo "Starting Apache HTTPD"
 httpd-foreground
