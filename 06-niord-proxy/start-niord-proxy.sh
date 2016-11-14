5a59474cb40b58b943ec1454fba9abc55e4c0b1b
peder280370 <peder@carolus.dk>
2016-11-14 13:54:25 +0100

    Add support for execution mode

--- 06-niord-proxy/start-niord-proxy.sh
+++ 06-niord-proxy/start-niord-proxy.sh
@@ -3,6 +3,7 @@
 NIORD_PROXY_SERVER=https://niord.e-navigation.net
 NIORD_HOME=${NIORD_HOME:-~/.niord}
 NIORD_REPO_PATH=/opt/niord/repo
+EXECUTION_MODE=development
 
 VERSION=${1:-1.0.0}
 DOCKER_TAG="dmadk/niord-proxy:$VERSION"
@@ -14,6 +15,7 @@ docker pull $DOCKER_TAG
 docker run \
   --name niord-proxy \
   -p 9090:8080 \
+  -e EXECUTION_MODE=$EXECUTION_MODE \
   -e NIORD_PROXY_SERVER=$NIORD_PROXY_SERVER \
   -e NIORD_REPO_PATH=$NIORD_REPO_PATH \
   -v $NIORD_HOME:/opt/niord \
