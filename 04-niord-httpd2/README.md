d1622de197c1a3f0ed87b97992c035a13469946a
peder280370 <peder@carolus.dk>
2017-08-14 08:32:21 +0200

    Try to fix an issue when running on RHEL

---
+++ 04-niord-httpd2/README.md
@@ -0,0 +1,9 @@
+# niord-httpd2
+
+A niord-httpd variant used temporarily while migrating from one production system to another.
+
+Should be deleted once the migration has been completed.
+
+The temporary hack is to add a an additional virtual host for
+niord-dma.e-navigation.net
+ 
---
+++ 04-niord-httpd2/build-niord-httpd.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ "$1" = "build" ]; then
+
+  echo "**********************************************"
+  echo "** Building Niord HTTPD                     **"
+  echo "**********************************************"
+
+  VERSION=${2:-1.0.0}
+  DOCKER_TAG="dmadk/niord-httpd2:$VERSION"
+  echo "**********************************************"
+  echo "** Building $DOCKER_TAG         **"
+  echo "**********************************************"
+  docker build --no-cache -t $DOCKER_TAG .
+
+  exit
+
+
+elif [ "$1" = "push" ]; then  
+  VERSION=${2:-1.0.0}
+  DOCKER_TAG="dmadk/niord-httpd2:$VERSION"
+  echo "Pushing $DOCKER_TAG to docker.io - make sure you are logged in"
+  docker push $DOCKER_TAG
+  exit     
+
+
+else
+    echo Unknown target: "$1"
+    echo Valid targets are:
+fi
+
+echo "  build <version>   Builds the specified version"
+echo "  push  <version>   Pushes version to docker.io"
+
+
