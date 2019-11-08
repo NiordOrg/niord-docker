6b560d9df078c5e350fccdff94ec30f41de00845
Andreas Borchsenius Westh <andreas@lundogbendsen.dk>
2019-11-08 14:57:05 +0100

    bash.

--- boot-up-niord.sh
+++ boot-up-niord.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Booting up niord"
 docker run  --name niord-smtp  -p 1025:25  -e SMTP_SERVER=changethis.dma.dk -e SERVER_HOSTNAME=andchangethis.dma.dk --rm --detach  dmadk/niord-smtp:1.0.0
-sh 01-niord-mysql/start-all.sh
-sh 02-niord-appsrv/start-niord-appsrv.sh
-sh 03-niord-keycloak/start-niord-keycloak.sh
-sh 04-niord-httpd/start-niord-httpd.sh
-sh 06-niord-proxy/start-niord-proxy.sh
+./01-niord-mysql/start-all.sh
+./02-niord-appsrv/start-niord-appsrv.sh
+./03-niord-keycloak/start-niord-keycloak.sh
+./04-niord-httpd/start-niord-httpd.sh
+./06-niord-proxy/start-niord-proxy.sh
 echo "bootup complete"
 docker ps -a
